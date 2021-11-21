41f46d549ce686b73c48a829f273a315ac9251c6
Falak Amin <falakamin2000@gmail.com>
2021-11-22 02:34:35 +0500

    Improved the UX for profile page

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,6 +4,7 @@ import { logout } from "../authentication/emailAndPassword";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useHistory } from 'react-router';
 import { auth } from "../libraries/firebase"
+import {Link} from 'react-router-dom'
 
 
 
@@ -17,12 +18,16 @@ export const NavigationBar = (props) => {
     return (
         <div className="navigation_bar">
             <div className="navigation_bar_links">
+                <Link to="/">
                 <div className="navbar_logo">
                     <img src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1603986512881x847913222560032400%2Fshipfair%2520core%2520logo%25201-02.png?w=128&h=128&auto=compress&fit=crop&dpr=2" alt="Shipfair" />
                 </div>
+                </Link>
                 <div className="navbar_links">
                     <ul>
+                        <Link to ="/">
                         <li>HOME</li>
+                        </Link>
                         <li>PACKAGES</li>
                         <li>TRIPS</li>
                         <li>CONTACT</li>
